680ca36003ecf364d224482c209c8fd09119bc17
vihar <vihar.kurama@gmail.com>
2019-01-02 10:01:33 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mastering GO Lang!
+
+### The traditional program, "Hello World!"
+
+```go
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world")
+}
+```
+The keyword “import” is used for importing a package into other packages.
+In our first program we have imported the package “fmt” for using the function Println. 
+Println is the function used to print strings in "go" similar to `print()` in Python.
+The package “fmt” comes from the Go standard library. 
---
+++ variables.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main(){
+    var name = "Start"
+    fmt.Println(name)
+
+    var number int=1
+    fmt.Println(number)
+
+    // Initiliases the number to Zero.
+    var a int
+    fmt.Println(un_init)
+
+    var bool_value = true
+    fmt.Println(bool_value)
+}
+
+/*
+
+In Go we declare variables using the keyword `var` followed by the variable name,
+the typecasting is done automaticaly else the datatype can be declared after the
+variable name.
+
+Go Lang has four main datatypes namely,
+
+* Boolean
+* Numeric
+* String
+* Derived
+
+In each of the datatypes we have different allocations such as (true, false, uint8, uint16...)
+In dervied we have special datatypes consisting all pointers, arrays, unions, functions,
+slices, interfaces, maps and few other. 
+
+*/
